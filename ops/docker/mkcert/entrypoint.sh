c7f9b3ac9e022fcb6d9539d193178f5d31c7eeee
Ryan Grippeling <R.Grippeling@hotmail.com>
2024-06-15 11:18:34 +0200

    Moved configs to where they can be seen, and fixed docker-compose up with the correct configs. Dynamic doesn't work yet.

--- ops/docker/mkcert/entrypoint.sh
+++ ops/docker/mkcert/entrypoint.sh
@@ -7,5 +7,10 @@ export CAROOT=/root/.local/share/mkcert
 
 mkcert -cert-file /certitificate-data/cert.pem -key-file /certitificate-data/key.pem "*.test" "traefik.test" "*.traefik.test"
 
+# build ssl.yml
+
+# Signal that the service is healthy
+touch /certitificate-data/healthy
+
 #Keep running
 tail -f /dev/null
